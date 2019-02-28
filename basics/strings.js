fb4dfcab26cf5976b0eb4b4e50d25176f90a08e3
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-27 16:26:46 -0800

    initial commit, created some basic examples

---
+++ basics/strings.js
@@ -0,0 +1,12 @@
+let firstName = 'Frank';
+let lastName = 'Donnelly';
+
+let fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+let city = 'San Diego';
+let country = 'United States';
+let location = `${city}, ${country}`;
+
+console.log(location);
