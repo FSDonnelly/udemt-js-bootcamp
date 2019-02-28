20a6c5131fd933788e9d5ad7d1c21e9bc926f409
FSDonnelly <donnelly_frank@yahoo.com>
2019-02-27 16:47:26 -0800

    examples on variables and numbers in numbers.js

--- basics/numbers.js
+++ basics/numbers.js
@@ -1,3 +1,30 @@
-let num = 5.5;
+let num1 = 5 * 2; // multiply
+console.log(`Multiply 5 x 2 = ${num1}.`);
+let num2 = 8 / 2; // divide
+console.log(`Divide 8 by 2 = ${num2}`);
+let num3 = 9 + 2; // add
+console.log(`Add 9 + 2 = ${num3}`);
+let num4 = 3 - 2; // subtract
+console.log(`Subtract 3 - 2 = ${num4}`);
+let num5 = 5 % 2; // modulus (division remainder)
+console.log(`% shows the remainder of 5 diveded by 2 (Modulus)= ${num5}`);
 
-console.log(num);
+
+// Example
+let age = 38;
+let dogYears = (age + 1) / 7;
+
+console.log(`I am ${dogYears} years old in dog years.`);
+
+// Challenge area
+
+// studentScore = 18
+// maxScore = 20
+// percent 90
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = (studentScore / maxScore) * 100;
+
+console.log(percent);
+
