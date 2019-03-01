1a96dada4ee683f4c3074ee2655c035d6d59da41
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-01 12:09:03 -0800

    examples of undefined and null

---
+++ functions/undefined-null.js
@@ -0,0 +1,25 @@
+// Undefined for variable
+let name
+
+name = 'Jen'
+
+if (name === undefined) {
+    console.log('Pleaseprovide a name')
+}else{
+    console.log(name)
+}
+
+// Undefined for function arguments
+let square = function (num) {
+    console.log(num)
+}
+
+let result = square()
+console.log(result)
+
+// Null as assigned value
+let age = 27
+
+age = null
+
+console.log(age)
