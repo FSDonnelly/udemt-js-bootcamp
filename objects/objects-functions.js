0d4ed88d9342a62337f55319bb7eeac32857fff8
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-03 17:20:17 -0800

    examples of objects in functions

---
+++ objects/objects-functions.js
@@ -0,0 +1,39 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+let otherBook = {
+    title: 'A Peoples History',
+    author: 'Howard Zinn',
+    pageCount: 723
+}
+
+let getSummary = function (book) {
+    return {
+        summary: `${book.title} by ${book.author}`,
+        pageCountSummary: `${book.title} is ${book.pageCount} pages long`
+    }
+}
+
+let bookSummary = getSummary(myBook)
+let otherBookSummary = getSummary(otherBook)
+
+console.log(bookSummary.pageCountSummary, bookSummary.summary)
+console.log(otherBookSummary.pageCountSummary, otherBookSummary.summary)
+
+// Challenge Area
+
+// Create funciton - take in - return object with all three temps convert
+
+let convertFarenheit = function (farenheit) {
+    return {
+        farenheit: farenheit,
+        kelvin: (farenheit + 459.67) *(5/9),
+        celsius: (farenheit - 32) * (5/9)
+    }
+}
+
+let temps = convertFarenheit(74)
+console.log(temps)
