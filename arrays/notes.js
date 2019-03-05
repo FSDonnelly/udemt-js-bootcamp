ebedb79fd553963efcdde16015af70804da83d5d
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-04 21:03:42 -0800

    created example using indexOf and findIndex to access items in an array

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{}, {
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise, Eating better'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 // console.log(notes.pop())// removes last item if none specified
 // notes.push('My new note') // adds to end of array
@@ -6,26 +15,36 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 // console.log(notes.shift())// removes first item
 // console.log(notes.unshift('My first note'))// adds new item to first on the list
 
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
-notes.forEach(function (item, index) {
-    console.log(item)
-    console.log(index)
-})
+// notes.forEach(function (item, index) {
+//     console.log(item)
+//     console.log(index)
+// })
 
 console.log(notes.length)
 console.log(notes)
 
+
+let someObject = {}
+let otherObject = someObject
+console.log(someObject === otherObject)
+const index = notes.findIndex(function (note, index){
+    console.log(note)
+    return note.title === 'Habbits to work on'
+})
+console.log(index)
+
 // Counting.... 
-for (let i = 0; i <= 2; i++) {
-    console.log(i)
-}
+// for (let i = 0; i <= 2; i++) {
+//     console.log(i)
+// }
 
-for (let i = 0; i < notes.length; i++) {
-    console.log(notes[i])
-}
+// for (let i = 0; i < notes.length; i++) {
+//     console.log(notes[i])
+// }
 
 
-for (let i = notes.length - 1; i >= 0; i--) {
-    console.log(notes[i])
-}
+// for (let i = notes.length - 1; i >= 0; i--) {
+//     console.log(notes[i])
+// }
