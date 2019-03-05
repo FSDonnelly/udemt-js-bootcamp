4bacd647e2b76dcca441b734f20c190ed3fb9492
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-04 20:44:17 -0800

    created examples of using for loop in an array

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,10 +1,10 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
-console.log(notes.pop())// removes last item if none specified
-notes.push('My new note') // adds to end of array
+// console.log(notes.pop())// removes last item if none specified
+// notes.push('My new note') // adds to end of array
 
-console.log(notes.shift())// removes first item
-console.log(notes.unshift('My first note'))// adds new item to first on the list
+// console.log(notes.shift())// removes first item
+// console.log(notes.unshift('My first note'))// adds new item to first on the list
 
 notes[2] = 'This is now the new note 3'
 
@@ -15,3 +15,17 @@ notes.forEach(function (item, index) {
 
 console.log(notes.length)
 console.log(notes)
+
+// Counting.... 
+for (let i = 0; i <= 2; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i < notes.length; i++) {
+    console.log(notes[i])
+}
+
+
+for (let i = notes.length - 1; i >= 0; i--) {
+    console.log(notes[i])
+}
--- arrays/todos.js
+++ arrays/todos.js
@@ -9,3 +9,17 @@ todos.forEach(function (todo, index) {
     const num = index + 1
     console.log(`${num}. ${todo}`)
 })
+
+
+for (let i = 0; i < todos.length; i++) {
+     const num = i + 1
+     const todo = todos[i]
+    console.log(`${num}. ${todo}`)
+}
+
+
+for (let i = todos.length - 1; i >= 0; i--) {
+    const num = i + 1
+    const todo = todos[i]
+   console.log(`${num}. ${todo}`)
+}
