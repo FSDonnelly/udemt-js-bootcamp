d04e3ba41cab18b69bc4eec91d1812b168a12a83
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-05 20:00:34 -0800

    examples of sort method with arrays

--- arrays/notes.js
+++ arrays/notes.js
@@ -9,6 +9,18 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const sortNotes = function (notes) {
+    notes.sort(function (a, b) {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
 const findNote = function (notes, noteTitle) {
     return notes.find(function (note, index) {
         return note.title.toLowerCase() === noteTitle.toLowerCase()
@@ -23,9 +35,14 @@ const findNotes = function (notes, query) {
     })
 }
 
-console.log(findNotes(notes, 'eating'))
-const note = findNote(notes, 'some other office modification')
-console.log(note)
+
+
+sortNotes(notes)
+console.log(notes)
+
+// console.log(findNotes(notes, 'eating'))
+// const note = findNote(notes, 'some other office modification')
+// console.log(note)
 // console.log(notes.length)
 // console.log(notes)
 
