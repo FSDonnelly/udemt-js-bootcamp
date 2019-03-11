34186f7bd24788c90feba84313054898127a5ce1
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-10 21:21:36 -0700

    created oop (object oriented programming) folder with example of constructor in hangmanjs

--- oop/person.js
+++ oop/person.js
@@ -1,7 +1,12 @@
-const Person = function (firstName) {
+const Person = function (firstName, lastName, age) {
     this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
 }
 
-const me = new Person('Frank')
-
+const me = new Person('Frank', 'Donnelly', 38)
+me.firstName = 'Steve'
 console.log(me)
+
+const person2 = new Person('Clansy', 'Turner', 22)
+console.log(person2)
