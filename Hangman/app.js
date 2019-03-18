67ff846af5d4bbbc640ff3fdd092723ed4e79ab6
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-17 20:44:34 -0700

    added fetch() to hangman to get puzzle data

--- Hangman/app.js
+++ Hangman/app.js
@@ -18,22 +18,11 @@ getPuzzle(`2`).then((puzzle) => {
     console.log(`Error: ${error}`)
 }) 
 
-// getCountry('US').then((country) => {
-//     console.log(country.name)
-// }, (error) => {
-//     console.log(`Error: ${error}`)
-// })
+getCountry('BR').then((country) => {
+    console.log(country.name)
+}).catch((error) => {
+    console.log(`Error: ${error}`)
+})
 
-// fetch(`http://puzzle.mead.io/puzzle`, {}).then((response) => {
-//     if (response.status === 200) {
-//         return response.json()
-//     } else {
-//         throw new Error(`Unable to fetch the puzzle data`)
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle)
-// }).catch((error) => {
-//     console.log(error)
-// })
 
 
