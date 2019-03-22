451c69ab3920d9b5accd9bbfbdac3310730fa077
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-22 16:34:02 -0700

    examples of export and import in boilerplate folder

---
+++ boilerplate/src/index.js
@@ -0,0 +1,8 @@
+// import './utilities'
+import { add, name } from './utilities'
+import { scream } from './scream'
+
+console.log('index.js')
+console.log(add(32, 1))
+console.log(name)
+console.log(scream('hello'))
