2ab3ea2eb7c2b50b4741e865d539c250adbb166d
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-23 19:31:32 -0700

    started to re-structure notes-app

---
+++ notes-app/src/notes.js
@@ -0,0 +1,43 @@
+import uuidv4 from 'uuid/v4'
+import moment from 'moment'
+
+let notes = []
+
+// Check for existing saved data
+const loadNotes = () => {
+    const notesJSON = localStorage.getItem('notes')
+
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []
+    } catch (e) {
+        return []
+    }
+}
+
+// Save the notes to local storage
+const saveNotes = () => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+// Expose notes from module
+const getNotes = () => notes
+
+const createNote = () => {
+    const id = uuidv4()
+    const timestamp = moment().valueOf()
+
+    notes.push({
+        id: id,
+        title: '',
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
+    })
+    saveNotes()
+}
+
+
+
+notes = loadNotes()
+
+export { getNotes, createNote }
