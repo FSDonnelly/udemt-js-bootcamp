a6c8b562f9130cd53afc2e70c1c9ee46b6f23955
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-23 21:44:43 -0700

    re-structuring notes-app

--- notes-app/src/edit.js
+++ notes-app/src/edit.js
@@ -1,1 +1,35 @@
-console.log('edit.js')
+import { initializeEditPage, generateLastEdited } from './views'
+import { updateNote, removeNote } from './notes'
+
+const titleEl = document.querySelector('#note-title')
+const bodyEl = document.querySelector('#note-body')
+const removeEl = document.querySelector('#remove-note')
+const dateEl = document.querySelector('#last-edited')
+const noteId = location.hash.substring(1)
+
+initializeEditPage(noteId)
+
+titleEl.addEventListener('input', (e) => {
+    const note = updateNote(noteId, {
+        title: e.target.value
+    })
+    dateEl.textContent = generateLastEdited(note.updatedAt)
+})
+
+bodyEl.addEventListener('input', (e) => {
+    const note = updateNote(noteId, {
+        body: e.target.value
+    })
+    dateEl.textContent = generateLastEdited(note.updatedAt)
+})
+
+removeEl.addEventListener('click', (e) => {
+    removeNote(note.id)
+    location.assign('/index.html')
+})
+
+window.addEventListener('storage', (e) => {
+    if (e.key === 'notes') {
+        initializeEditPage(noteId)
+    }
+})
