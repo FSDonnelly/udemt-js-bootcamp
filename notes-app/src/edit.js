afb5ca1d5e9e6c0548d5adc81a220f3e247526e2
FSDonnelly <donnelly_frank@yahoo.com>
2019-03-24 20:28:55 -0700

    fixing bugs in edit.js from re-structure

--- notes-app/src/edit.js
+++ notes-app/src/edit.js
@@ -6,30 +6,48 @@ const bodyEl = document.querySelector('#note-body')
 const removeEl = document.querySelector('#remove-note')
 const dateEl = document.querySelector('#last-edited')
 const noteId = location.hash.substring(1)
+let notes = getSavedNotes()
+let note = notes.find((note) => note.id === noteId)
 
-initializeEditPage(noteId)
+if (!note) {
+    location.assign('/index.html')
+}
+
+titleEl.value = note.title
+bodyEl.value = note.body
+dateEl.textContent = generateLastEdited(note.updatedAt) 
 
 titleEl.addEventListener('input', (e) => {
-    const note = updateNote(noteId, {
-        title: e.target.value
-    })
-    dateEl.textContent = generateLastEdited(note.updatedAt)
+    note.title = e.target.value
+    note.updatedAt = moment().valueOf()
+    dateEl.textContent = generateLastEdited(note.updatedAt) 
+    savedNotes(notes)
 })
 
 bodyEl.addEventListener('input', (e) => {
-    const note = updateNote(noteId, {
-        body: e.target.value
-    })
-    dateEl.textContent = generateLastEdited(note.updatedAt)
+    note.body = e.target.value
+    note.updatedAt = moment().valueOf()
+    dateEl.textContent = generateLastEdited(note.updatedAt) 
+    savedNotes(notes)
 })
 
 removeEl.addEventListener('click', (e) => {
-    removeNote(note.id)
-    location.assign('/index.html')
+   removeNote(note.id)
+   savedNotes(notes)
+   location.assign('/index.html')
 })
 
 window.addEventListener('storage', (e) => {
     if (e.key === 'notes') {
-        initializeEditPage(noteId)
+        notes = JSON.parse(e.newValue)
+        note = notes.find((note) => note.id === noteId)
+        
+        if (!note) {
+            location.assign('/index.html')
+        }
+        
+        titleEl.value = note.title
+        bodyEl.value = note.body
+        dateEl.textContent = generateLastEdited(note.updatedAt) 
     }
 })
